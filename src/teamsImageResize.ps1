1c59fbc8a2e54f667cd7f4dd099ec7bc1feba4e8
Julian <39803323+NHS-juju@users.noreply.github.com>
2025-02-04 15:23:46 +0000

    Create teamsImageResize.ps1

---
+++ src/teamsImageResize.ps1
@@ -0,0 +1,32 @@
+$backgroundsdir = ""
+$renamedbackgrounds = New-Item -Path "$backgroundsdir" -Name Output -ItemType Directory -Force
+$useGuid = 0
+
+Function ResizeImage($file, $output) {
+    $wia = New-Object -com wia.imagefile
+    $wia.LoadFile($file)
+    $wip = New-Object -ComObject wia.imageprocess
+    $scale = $wip.FilterInfos.Item("Scale").FilterId                    
+    $wip.Filters.Add($scale)
+    $wip.Filters[1].Properties("MaximumWidth") = 150
+    $wip.Filters[1].Properties("MaximumHeight") = 150
+    $wip.Filters[1].Properties("PreserveAspectRatio") = $true
+    $wip.Apply($wia) 
+    $newimg = $wip.Apply($wia)
+    $newimg.SaveFile($output)
+}
+
+# #Rename and resize files
+$images = get-childitem $backgroundsdir | Where-Object { $_.Name -Like "*.jpg" -or $_.Name -Like "*.jpeg" -or $_.Name -Like "*.png" -or $_.Name -Like "*.bmp" }
+foreach ($image in $images) {
+    $fileExtension = [System.IO.Path]::GetExtension($image)
+    if ($useGuid -eq 1){$baseName = new-guid}
+    else{$baseName = [System.IO.Path]::GetFileNameWithoutExtension($image)}
+
+    $mainName = "$baseName$fileExtension"
+    $thumbname = "${baseName}_thumb$fileExtension"
+    $target = Join-Path -Path $renamedbackgrounds -ChildPath $mainName
+    $thumbTarget = Join-Path -Path $renamedbackgrounds -ChildPath $thumbname
+    Copy-Item -Path $image.FullName -Destination $target
+    ResizeImage $image $thumbTarget
+}
