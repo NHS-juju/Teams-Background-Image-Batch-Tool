29056811dd6730ccfe5b2a10d6f61385d1b65910
Julian <39803323+NHS-juju@users.noreply.github.com>
2025-05-19 15:18:21 +0100

    Update teamsImageResize.ps1
    Values for the directory containing the images is now passed to script instead of being manually added in.
    Flag added for automatically writing the output to the Teams appdata location
    Fixed bug where the wia calls were only given the filename so the resize would fail if the script was being run from anywhere other than the directory containing the files.

--- src/teamsImageResize.ps1
+++ src/teamsImageResize.ps1
@@ -1,5 +1,11 @@
-$backgroundsdir = ""
-$renamedbackgrounds = New-Item -Path "$backgroundsdir" -Name Output -ItemType Directory -Force
+param ($sourceDirectory, $autoCopyToTeamsFolder = 'N')
+$trueArray = 1,'Y','YES','T'.'TRUE'
+if ($autoCopyToTeamsFolder -in $trueArray){
+    $targetDirectory = "$Env:USERPROFILE\AppData\Local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads"
+}
+else {
+    $targetDirectory = New-Item -Path "$sourceDirectory" -Name Output -ItemType Directory -Force
+}
 
 Function ResizeImage($file, $output) {
     $wia = New-Object -com wia.imagefile
@@ -16,14 +22,14 @@ Function ResizeImage($file, $output) {
 }
 
 # #Rename and resize files
-$images = get-childitem $backgroundsdir | Where-Object { $_.Name -Like "*.jpg" -or $_.Name -Like "*.jpeg" -or $_.Name -Like "*.png" -or $_.Name -Like "*.bmp" }
+$images = get-childitem $sourceDirectory | Where-Object { $_.Name -Like "*.jpg" -or $_.Name -Like "*.jpeg" -or $_.Name -Like "*.png" -or $_.Name -Like "*.bmp" }
 foreach ($image in $images) {
     $fileExtension = [System.IO.Path]::GetExtension($image)
     $baseName = new-guid
     $mainName = "$baseName$fileExtension"
     $thumbname = "${baseName}_thumb$fileExtension"
-    $target = Join-Path -Path $renamedbackgrounds -ChildPath $mainName
-    $thumbTarget = Join-Path -Path $renamedbackgrounds -ChildPath $thumbname
+    $target = Join-Path -Path $targetDirectory -ChildPath $mainName
+    $thumbTarget = Join-Path -Path $targetDirectory -ChildPath $thumbname
     Copy-Item -Path $image.FullName -Destination $target
-    ResizeImage $image $thumbTarget
+    ResizeImage $image.FullName $thumbTarget
 }
